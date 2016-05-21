0855ed783e842f20d972c3003555f63a9b678f61
Matthew Johnson <msjohn11@ncsu.edu>
2016-05-21 05:58:53 -0400

    Initial Commit

---
+++ Spindler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spindler
+//
+//  Created by Scott Johnson on 5/21/16.
+//  Copyright © 2016 Scott Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
